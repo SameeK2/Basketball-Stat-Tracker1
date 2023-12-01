d7ef23388af5c0731ac57ae0b5602b8bfd228a5c
SameeK2 <146093450+SameeK2@users.noreply.github.com>
2023-11-30 19:35:54 -0500

    Create BasketballStats.java

---
+++ BasketballStats.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class BasketballStats {
+    private int points;
+    private int rebounds;
+    private int assists;
+    private String name;
+
+    public BasketballStats() {
+        points = 0;
+        rebounds = 0;
+        assists = 0;
+        name = "lebron";
+    }
+
+    public BasketballStats(int points, int rebounds, int assists, String name) {
+        this.points = points;
+        this.rebounds = rebounds;
+        this.assists = assists;
+        this.name = name;
+    }
+
+    public void recordPoints(int points) {
+        this.points += points;
+    }
+
+    public void recordRebounds(int rebounds) {
+        this.rebounds += rebounds;
+    }
+
+    public void recordAssists(int assists) {
+        this.assists += assists;
+    }
+
+    public void recordName(String name){
+      this.name = name;
+    }
+
+    public void displayStats() {
+        System.out.println("\n" + this.toString());
+    }
+
+    public void rerankStats() {
+        int[] statsArray = {points, rebounds, assists};
+        Arrays.sort(statsArray);
+        points = statsArray[0];
+        rebounds = statsArray[1];
+        assists = statsArray[2];
+  }
+    public String toString(){
+        String point1 = "Points:" + points + "\n";
+        String rebound1 = "Rebounds:" + rebounds + "\n";
+        String assist1 = "Assists:" + assists + "\n";
+        String name1 = "Name:" + name;
+        return point1 + rebound1 + assist1 + name1;
+    }
+
+    public void generateAndDisplayJerseyNumber() {
+        int jerseyNumber = (int) (Math.random() * 100 + 1);  
+        System.out.println("Randomly generated jersey number: " + jerseyNumber);
+        String jerseyStr = String.valueOf(jerseyNumber);
+        System.out.println("Length of jersey number: " + jerseyStr.length());
+    }
+  
+}
